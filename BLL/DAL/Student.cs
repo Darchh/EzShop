872c6e313bde04a217fe991c4509d6c3556b55ed
Berkay <brkymdn@gmail.com>
2024-10-08 20:36:52 +0300

    Add project files.

---
+++ BLL/DAL/Student.cs
@@ -0,0 +1,16 @@
+﻿#nullable disable
+
+namespace BLL.DAL
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+
+        public decimal GPA { get; set; }
+    }
+}
